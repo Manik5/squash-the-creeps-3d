e54210c50a1b904c9bf722a47287ae2cc22be0d3
Manik A A Ngurah <manik.aangurah@gmail.com>
2022-01-23 13:48:51 +0100

    update

--- Player.gd
+++ Player.gd
@@ -18,12 +18,17 @@ func _physics_process(delta):
 	if Input.is_action_pressed("move_back"):
 		direction.z += 1
 	if Input.is_action_pressed("move_forward"):
-		direction.z += 1
+		direction.z -= 1
 		
 	if direction != Vector3.ZERO:
 		direction = direction.normalized()
+		$Pivot.look_at(translation + direction, Vector3.UP)
 		
 	velocity.x = direction.x * speed
 	velocity.z = direction.z * speed
 	
+	if is_on_floor() and Input.is_action_pressed("jump"):
+		velocity.y += jump_impulse
+		
+	velocity.y -= fall_acceleraton * delta
 	velocity = move_and_slide(velocity, Vector3.UP)
